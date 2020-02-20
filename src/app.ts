b431f7c84d0538e456b80b6fa56e7ca018edb9e2
hn_ryanb <rbrown_1193@hotmail.com>
2020-02-21 09:34:44 +1300

    Dockerize application

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import { Application, Request, Response } from "express";
+import { Application, NextFunction, Request, Response } from "express";
 import e = require("express");
 
 const app: Application = e();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+app.get('/', (req: Request, res: Response) => res.send("Hello {req.query.name}!"));
+
+app.use(function (req: Request, res: Response, next: NextFunction) {
+    res.status(404).send("Page not found")
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
