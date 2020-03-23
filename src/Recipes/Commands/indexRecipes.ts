aaaa7b93f3baedfb8728774d15d478e87d2c1fb6
hn_ryanb <rbrown_1193@hotmail.com>
2020-03-23 17:01:10 +1300

    Fixed index recipes command output

--- src/Recipes/Commands/indexRecipes.ts
+++ src/Recipes/Commands/indexRecipes.ts
@@ -1,33 +1,23 @@
 import Config from '../../lib/Config'
 import Recipe from '../RecipeModel'
 import Client from '../../lib/SearchClient'
-import { ResponseError } from '@elastic/elasticsearch/lib/errors'
-import { Index } from '@elastic/elasticsearch/api/requestParams'
 
 export default async function indexRecipes() {
     const recipes = await Recipe.query()
-    console.log(recipes)
-    recipes.forEach(async (recipe) => {
-        try {
-            const req: Index = {
-                id: recipe.id.toString(),
-                index: Config.ELASTICSEARCH_RECIPES_INDEX,
-                op_type: 'index',
-                body: {
-                    user_id: recipe.user_id,
-                    name: recipe.name,
-                    description: recipe.description,
-                    cooking_time: recipe.cooking_time,
-                    ingredients: recipe.ingredients,
-                }
+    Promise.all(recipes.map(async (recipe) => {
+        Client.index({
+            id: recipe.id.toString(),
+            index: Config.ELASTICSEARCH_RECIPES_INDEX,
+            op_type: 'index',
+            body: {
+                user_id: recipe.user_id,
+                name: recipe.name,
+                description: recipe.description,
+                cooking_time: recipe.cooking_time,
+                ingredients: recipe.ingredients,
             }
-            await Client.index(req)
-            console.log(`${recipe.name} indexed successfully`)
-        } catch (error) {
-            if (error instanceof ResponseError) {
-                console.log(error.message)
-            }
-        }
-    })
+        })
+        console.log(`${recipe.name} indexed successfully`)
+    }))
     await Client.indices.refresh({ index: 'recipes' })
 }
